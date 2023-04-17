ff28608ae0331b3fc3fd41470239de66c58670c4
shonifari <karis.omotosho@gmail.com>
2023-04-17 18:10:40 +0100

    first set up

---
+++ test/client.js
@@ -0,0 +1,3 @@
+import { Fobal } from "fobal-ts";
+const fobal = new Fobal("apiKey");
+console.log(fobal.apiKey);
---
+++ test/server.js
@@ -0,0 +1,3 @@
+const {Fobal} = require("fobal-ts");
+const fobal = new Fobal("apiKey");
+console.log(fobal.apiKey);
